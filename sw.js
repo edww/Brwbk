a82c2d87b395f76a124ff65cd2856ac314a52e61
edww <37853188+edww@users.noreply.github.com>
2026-06-29 11:47:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-// Service worker volontairement neutre en V5 : pas de cache, suppression des anciens caches.
 self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request, {cache: 'no-store'}).catch(() => fetch(event.request)));
-});
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => event.respondWith(fetch(event.request, {cache:'no-store'}).catch(() => fetch(event.request))));
